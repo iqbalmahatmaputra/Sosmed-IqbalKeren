a6df72029cd30522c92ceba68340c9e3c385e4f2
Iqbal Mahatma Putra <iqbal@alumni.pcr.ac.id>
2024-05-07 17:04:06 +0700

    Feed Photo

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content : ["./**/**/*.{html,js}"],
+  theme: {
+    container: {
+      center:true,
+      padding:'16px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
